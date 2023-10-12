483c6aa014398d59afac4d699dd76e19550aa4bc
Rishabh Pandey <rishabh@walkover.in>
2023-10-12 22:01:34 +0530

    Theme changer

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { ThemeBtn, Card } from "./components";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // Actual change in theme
+  useEffect(() => {
+    const htmlElement = document.querySelector("html");
+    htmlElement.classList.remove("dark", "light");
+    htmlElement.classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <>
+      <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+        <div className="flex radial-blue flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn />
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
